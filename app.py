e778bd37c058d5e5ec85d33cdfddee8ca121049c
Kathleen Mercado <kamercado@outlook.com>
2020-02-12 21:18:08 -0800

    started app.py

--- app.py
+++ app.py
@@ -1,33 +1,51 @@
-import os
-
-import pandas as pd
+#import dependencies
 import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-
+import pickle
 from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
 
+#app setup
 app = Flask(__name__)
 
-#################################################
-# Database Setup
-#################################################
+##########################
+# Web pages of interface #
+##########################
 
+#App routes to render HTML templates/web pages
 @app.route("/")
 def index():
-    """Return the homepage."""
+    #Return the homepage/form
     return render_template("index.html")
 
 @app.route("/nlp")
 def nlp():
-    """Return the homepage."""
+    #Return the How to Improve Your Ratings page
     return render_template("nlp.html")
     
 @app.route("/geomap")
 def geomap():
-    """Return the homepage."""
+    #Return the Geomap displaying other Airbnbs in the area
     return render_template("geomap.html")
+
+##############################################################
+# Predicting the host Airbnb rating based on characteristics #
+# of their airbnb input by the user                          #
+##############################################################
+
+#load the trained model
+airbnb_model = pickle.load(open('model.pkl','rb'))
+
+#app route to create API to predict Airbnb host's rating
+@app.route('/predict',methods=['POST'])
+def predict():
+	data = request.get_json(force=True)
+	#make prediction of rating using the airbnb_model that was pickled
+	prediction = airbnb_model.predict([[np.array(data['exp'])]])
+	#rating (output) is first value of prediction
+	rating = prediction[0]
+	return jsonify(rating)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
