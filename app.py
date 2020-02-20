eb48c1aaea8df62cd7df38dfe896664d38112ab7
hanhmyle <43523575+hanhmyle@users.noreply.github.com>
2020-02-19 18:32:21 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 #import dependencies
 import numpy as np
 import pickle
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, session
 
 #app setup
 app = Flask(__name__)
+
+# secret key is needed for session
+app.secret_key = 'ucidcebc08190220'
 
 ##########################
 # Web page(s) of interface #
@@ -35,10 +38,10 @@ def predict():
 	prediction = house_model.predict(input_list)
 	#variable for output (predicted price), round for decimals
 	price = round(prediction[0],2)
+	#use session to be able to pull price for calculation form
+	session['est_price'] = price
 	#populate webpage with price predictioin
-	return render_template('userinput.html', prediction_text = 'Your house in Ames, Iowa will sell for approx. $ {}'.format(price))
-
-
+	return render_template('userinput.html', prediction_text = 'Your house in Ames, Iowa will sell for approx. ${}'.format(price))
 
 @app.route('/results',methods=['POST'])
 def result():
@@ -48,6 +51,18 @@ def result():
 	#price (output) is first value of prediction
 	output = prediction[0]
 	return jsonify(output)
+
+#app route to calculate ROI
+@app.route('/calculator', methods=['POST'])
+def calculator():
+	#pull price from previous query
+	est_price = session.get('est_price', None)
+	#grab investment value from form and convert to float
+	for x in request.form.values():
+		investment = float(x)
+	#calculate roi
+	roi = float(est_price) - float(investment)
+	return render_template('userinput.html', roi_text = 'Your expected return on investment (ROI) is approx. ${}'.format(roi))
 
 
 if __name__ == '__main__':
